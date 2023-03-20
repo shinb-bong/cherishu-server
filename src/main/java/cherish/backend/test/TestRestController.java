3cf19932f6ef2806a94561e292b92e7b85fa2d6a
coodori <daoh98@naver.com>
2023-03-20 16:18:52 +0900

    issue #21 shutdown graceful option + test

---
+++ src/main/java/cherish/backend/test/TestRestController.java
@@ -0,0 +1,24 @@
+package cherish.backend.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("test")
+public class TestRestController {
+
+    @GetMapping("excuteProcess/{processNum}")
+    public String process(@PathVariable int processNum) throws InterruptedException {
+        log.info("========================== Start Process -> Process Number: {}", processNum);
+        Thread.sleep(20000);
+        log.info("========================== End Process -> Process Number: {}", processNum);
+
+        return "Process Success !!";
+    }
+
+
+}
