eb716d98cb4eacdac7e90cab8de2f8baa393365b
coodori <daoh98@naver.com>
2023-03-21 13:37:32 +0900

    #27-ErrorResponse & test

--- src/main/java/cherish/backend/common/exception/GlobalExceptionHandler.java
+++ src/main/java/cherish/backend/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package cherish.backend.common.exception;
 import cherish.backend.common.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,7 +15,19 @@ public class GlobalExceptionHandler {
     // 자바 빈 검증 예외 처리
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity handleValidException(MethodArgumentNotValidException e){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDto(e.getMessage()));
+        BindingResult bindingResult = e.getBindingResult();
+
+        StringBuilder builder = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            builder.append("[");
+            builder.append(fieldError.getField());
+            builder.append("](은)는 ");
+            builder.append(fieldError.getDefaultMessage());
+            builder.append(" 입력된 값: [");
+            builder.append(fieldError.getRejectedValue());
+            builder.append("]");
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDto(builder.toString()));
     }
 
     // 공통 예외 처리
