4be2cb9cc8014fffed98e5f33f29edd9e18bd90f
coodori <daoh98@naver.com>
2023-03-21 14:49:01 +0900

    PR#31 반영

--- src/main/java/cherish/backend/common/exception/GlobalExceptionHandler.java
+++ src/main/java/cherish/backend/common/exception/GlobalExceptionHandler.java
@@ -2,19 +2,35 @@ package cherish.backend.common.exception;
 
 import cherish.backend.common.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private ErrorResponseDto createError(Exception e) {
+        if (ObjectUtils.isEmpty(e.getMessage())) {
+            return new ErrorResponseDto("No error message from server.");
+        }
+        return new ErrorResponseDto(e.getMessage());
+    }
+
+    private ErrorResponseDto createError(String s) {
+        if (ObjectUtils.isEmpty(s)) {
+            return new ErrorResponseDto("No error message from server.");
+        }
+        return new ErrorResponseDto(s);
+    }
+
     // 자바 빈 검증 예외 처리
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity handleValidException(MethodArgumentNotValidException e){
+    public ErrorResponseDto handleValidException(MethodArgumentNotValidException e) {
         BindingResult bindingResult = e.getBindingResult();
 
         StringBuilder builder = new StringBuilder();
@@ -27,13 +43,13 @@ public class GlobalExceptionHandler {
             builder.append(fieldError.getRejectedValue());
             builder.append("]");
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDto(builder.toString()));
+        return createError(builder.toString());
     }
 
     // 공통 예외 처리
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public ResponseEntity handleException(Exception e){
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponseDto(e.getMessage()));
+    public ErrorResponseDto handleException(Exception e) {
+        return createError(e);
     }
-
 }
