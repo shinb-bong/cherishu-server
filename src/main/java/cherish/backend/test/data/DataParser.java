88fbcbbc977e5c328d10833fb8bf38534fd7fa5a
YongHyun Kim <dydgus.k@gmail.com>
2023-03-28 14:52:34 +0900

    DataRow 필드 string으로 변경

--- src/main/java/cherish/backend/test/data/DataParser.java
+++ src/main/java/cherish/backend/test/data/DataParser.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.FileReader;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -12,6 +13,9 @@ public class DataParser {
 
     public void read() {
         try (CSVReader csvReader = new CSVReader(new FileReader("data.csv"))) {
+            List<DataRow> dataList = csvReader.readAll().stream()
+                .map(DataRow::of)
+                .toList();
 
         } catch (Exception e) {
             log.error(e.getMessage(), e);
--- src/main/java/cherish/backend/test/data/DataRow.java
+++ src/main/java/cherish/backend/test/data/DataRow.java
@@ -2,12 +2,14 @@ package cherish.backend.test.data;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Slf4j
 @AllArgsConstructor
 @Data
 public class DataRow {
@@ -15,7 +17,7 @@ public class DataRow {
     private String brand;
     private String category;
     private Set<String> categoryChild;
-    private int price;
+    private String price;
     private String brandUrl;
     private String kakaoUrl;
     private String coupangUrl;
@@ -23,8 +25,8 @@ public class DataRow {
     private String description;
     private Set<String> job;
     private Set<String> jobChild;
-    private int minAge;
-    private int maxAge;
+    private String minAge;
+    private String maxAge;
     private Set<String> situation;
     private Set<String> emotion;
     private Set<String> gender;
@@ -41,23 +43,23 @@ public class DataRow {
             row[0],
             row[1],
             row[2],
-            Arrays.stream(row[3].split(",")).map(StringUtils::deleteWhitespace).collect(Collectors.toSet()),
-            Integer.parseInt(row[4]),
+            Arrays.stream(row[3].split(",")).map(String::trim).collect(Collectors.toSet()),
+            row[4],
             row[5],
             row[6],
             row[7],
             row[8],
             row[9],
-            Arrays.stream(row[10].split(",")).map(StringUtils::deleteWhitespace).collect(Collectors.toSet()),
-            Arrays.stream(row[11].split(",")).map(StringUtils::deleteWhitespace).collect(Collectors.toSet()),
-            Integer.parseInt(row[12]),
-            Integer.parseInt(row[13]),
-            Arrays.stream(row[14].split(",")).map(StringUtils::deleteWhitespace).collect(Collectors.toSet()),
-            Arrays.stream(row[15].split(",")).map(StringUtils::deleteWhitespace).collect(Collectors.toSet()),
-            Arrays.stream(row[16].split(",")).map(StringUtils::deleteWhitespace).collect(Collectors.toSet()),
-            Arrays.stream(row[17].split(",")).map(StringUtils::deleteWhitespace).collect(Collectors.toSet()),
-            Arrays.stream(row[18].split(",")).map(StringUtils::deleteWhitespace).collect(Collectors.toSet()),
-            Arrays.stream(row[19].split(",")).map(StringUtils::deleteWhitespace).collect(Collectors.toSet())
+            Arrays.stream(row[10].split(",")).map(String::trim).collect(Collectors.toSet()),
+            Arrays.stream(row[11].split(",")).map(String::trim).collect(Collectors.toSet()),
+            row[12],
+            row[13],
+            Arrays.stream(row[14].split(",")).map(String::trim).collect(Collectors.toSet()),
+            Arrays.stream(row[15].split(",")).map(String::trim).collect(Collectors.toSet()),
+            Arrays.stream(row[16].split(",")).map(String::trim).collect(Collectors.toSet()),
+            Arrays.stream(row[17].split(",")).map(String::trim).collect(Collectors.toSet()),
+            Arrays.stream(row[18].split(",")).map(String::trim).collect(Collectors.toSet()),
+            Arrays.stream(row[19].split(",")).map(String::trim).collect(Collectors.toSet())
         );
     }
 }
