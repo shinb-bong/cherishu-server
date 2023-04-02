cb3eecec364094a38929e1c5f3734e0b3a99805e
coodori <daoh98@naver.com>
2023-04-02 17:28:27 +0900

    validation 추가 수정

--- src/main/java/cherish/backend/member/dto/MemberFormDto.java
+++ src/main/java/cherish/backend/member/dto/MemberFormDto.java
@@ -1,6 +1,7 @@
 package cherish.backend.member.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -17,7 +18,7 @@ public class MemberFormDto {
     private String email; // 이메일
     @NotEmpty
     private String password; // 패스워드
-    @NotEmpty
+    @NotNull
     private boolean infoCheck; // 광고성 동의
     // 추가 정보
     private String gender; // 성별
