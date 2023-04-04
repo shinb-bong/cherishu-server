062e74a88a11f58b5a99505bf7b0128e2760b8ea
YongHyun Kim <dydgus.k@gmail.com>
2023-04-04 20:48:25 +0900

    Request dto validation 적용

--- src/main/java/cherish/backend/board/controller/PublicMonthlyBoardController.java
+++ src/main/java/cherish/backend/board/controller/PublicMonthlyBoardController.java
@@ -1,13 +1,14 @@
 package cherish.backend.board.controller;
 
 import cherish.backend.auth.security.CurrentUser;
+import cherish.backend.board.dto.MonthlyCurationRequestDto;
 import cherish.backend.board.dto.MonthlyCurationResponseDto;
 import cherish.backend.board.service.MonthlyBoardService;
 import cherish.backend.member.model.Member;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class PublicMonthlyBoardController {
     private final MonthlyBoardService monthlyBoardService;
 
     @GetMapping("/curation/{year}/{month}")
-    public List<MonthlyCurationResponseDto> monthlyCuration(@PathVariable int year, @PathVariable int month, @CurrentUser Member member) {
-        return monthlyBoardService.getMonthlyCurationList(year, month, member);
+    public List<MonthlyCurationResponseDto> monthlyCuration(@Valid MonthlyCurationRequestDto dto, @CurrentUser Member member) {
+        return monthlyBoardService.getMonthlyCurationList(dto.getYear(), dto.getMonth(), member);
     }
 }
