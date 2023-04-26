8e8598e88feceffad1f1f5670cc1d39250b3210f
lielocks <kari300084@gmail.com>
2023-04-26 15:32:01 +0900

    url 변경

--- src/main/java/cherish/backend/item/controller/PublicRecommendItemController.java
+++ src/main/java/cherish/backend/item/controller/PublicRecommendItemController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/public/recommendItem")
+@RequestMapping("/public/mainPage")
 public class PublicRecommendItemController {
     private final RecommendItemService recommendItemService;
 
