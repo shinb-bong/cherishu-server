c008124011ab00d52885b1a4257737a35eb11177
YongHyun Kim <dydgus.k@gmail.com>
2023-05-11 22:34:06 +0900

    add: 비동기 이메일 전송

--- src/main/java/cherish/backend/common/constant/CommonConstants.java
+++ src/main/java/cherish/backend/common/constant/CommonConstants.java
@@ -5,4 +5,5 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class CommonConstants {
     public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh-token";
+    public static final String MAIL_TASK_EXECUTOR_NAME = "mailTaskExecutor";
 }
