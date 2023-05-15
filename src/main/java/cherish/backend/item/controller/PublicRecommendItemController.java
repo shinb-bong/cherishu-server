7dde109bc8ca8ca48eb4429964aa26e1dcb30047
coodori <daoh98@naver.com>
2023-05-15 23:18:29 +0900

    actuator setting complete

--- src/main/java/cherish/backend/item/controller/PublicRecommendItemController.java
+++ src/main/java/cherish/backend/item/controller/PublicRecommendItemController.java
@@ -5,12 +5,14 @@ import cherish.backend.item.dto.RecommendItemResponseDto;
 import cherish.backend.item.service.RecommendItemService;
 import cherish.backend.member.model.Member;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/public/main")
@@ -19,6 +21,7 @@ public class PublicRecommendItemController {
 
     @GetMapping
     public List<RecommendItemResponseDto> getRecommendItems(@CurrentUser Member member) {
+        log.info("메인페이지 조회 = {}", member);
         return recommendItemService.getRecommendItem(member);
     }
 }
