d6d92b813140b27812df176be2565ef870d110b0
Yonghyun Kim <dydgus.k@gmail.com>
2023-05-17 15:04:33 +0900

    add: 로그 필터 추가

---
+++ src/main/java/cherish/backend/common/filter/RequestResponseLoggingFilter.java
@@ -0,0 +1,86 @@
+package cherish.backend.common.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+import org.springframework.web.util.WebUtils;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class RequestResponseLoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (StringUtils.startsWithIgnoreCase(request.getRequestURI(), "/monitor")) {
+            chain.doFilter(request, response);
+            return;
+        }
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+        long start = System.currentTimeMillis();
+        chain.doFilter(requestWrapper, responseWrapper);
+        long end = System.currentTimeMillis();
+
+        log.debug("""
+                {} {} {} - {}ms
+                Headers : {}
+                Request : {}
+                Response : {}
+                """,
+            request.getMethod(),
+            request.getRequestURI(),
+            responseWrapper.getStatus(),
+            end - start,
+            getHeaders(request),
+            getRequestBody(requestWrapper),
+            getResponseBody(responseWrapper));
+    }
+
+    private Map<String, String> getHeaders(HttpServletRequest request) {
+        Map<String, String> headerMap = new HashMap<>();
+
+        Enumeration<String> headerArray = request.getHeaderNames();
+        while (headerArray.hasMoreElements()) {
+            String headerName = headerArray.nextElement();
+            headerMap.put(headerName, request.getHeader(headerName));
+        }
+        return headerMap;
+    }
+
+    private String getRequestBody(ContentCachingRequestWrapper request) {
+        ContentCachingRequestWrapper wrapper = WebUtils.getNativeRequest(request, ContentCachingRequestWrapper.class);
+        if (wrapper != null) {
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length > 0) {
+                return new String(buf);
+            }
+        }
+        return "-";
+    }
+
+    private String getResponseBody(final HttpServletResponse response) throws IOException {
+        String payload = null;
+        ContentCachingResponseWrapper wrapper = WebUtils.getNativeResponse(response, ContentCachingResponseWrapper.class);
+        if (wrapper != null) {
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length > 0) {
+                payload = new String(buf);
+                wrapper.copyBodyToResponse();
+            }
+        }
+        return payload == null ? "-" : StringUtils.abbreviate(payload, 255);
+    }
+}
