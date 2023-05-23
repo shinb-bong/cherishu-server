cebd4614c1a45208dbdda1ead9f339d94fd91192
lielocks <kari300084@gmail.com>
2023-05-23 10:11:58 +0900

    [refactor] ver1 itemLike 중복으로 여러번 DB에 저장되는 이슈

--- src/main/java/cherish/backend/item/repository/RecommendItemRepository.java
+++ src/main/java/cherish/backend/item/repository/RecommendItemRepository.java
@@ -3,9 +3,12 @@ package cherish.backend.item.repository;
 import cherish.backend.item.dto.QRecommendItemQueryDto;
 import cherish.backend.item.dto.RecommendItemQueryDto;
 import cherish.backend.member.model.Member;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -23,16 +26,33 @@ public class RecommendItemRepository {
     private final JPAQueryFactory queryFactory;
 
     public List<RecommendItemQueryDto> getRecommendItemList(Member member) {
-        BooleanExpression like = member != null ? new CaseBuilder().when(itemLike.member.eq(member)).then(true).otherwise(false) : Expressions.asBoolean(false);
+        BooleanExpression like = Expressions.asBoolean(false);
 
-        return queryFactory
+        JPAQuery<RecommendItemQueryDto> query = queryFactory
                 .select(new QRecommendItemQueryDto(
                         recommend.id, item.id, item.name, item.brand, item.price, item.imgUrl, like, recommend.title, recommend.subTitle, recommend.linkParam)
                 )
                 .from(recommendItem)
                 .leftJoin(recommendItem.item, item)
-                .leftJoin(recommendItem.recommend, recommend)
-                .leftJoin(recommendItem.item.itemLikes, itemLike)
-                .fetch();
+                .leftJoin(recommendItem.recommend, recommend);
+
+        if (member != null) {
+            query = query.leftJoin(recommendItem.item.itemLikes, itemLike)
+                    .on(itemLike.member.eq(member).and(itemLike.item.eq(item)))
+                    .where(itemLike.id.in(
+                            JPAExpressions.select(itemLike.id)
+                                    .from(itemLike)
+                                    .where(itemLike.member.eq(member), itemLike.item.eq(item))
+                                    .groupBy(itemLike.item.id)
+                                    .having(itemLike.item.id.count().gt(1))
+                    ));
+            like = new CaseBuilder()
+                    .when(itemLike.id.isNotNull())
+                    .then((Predicate) Expressions.asBoolean(true))
+                    .otherwise(Expressions.asBoolean(false));
+        }
+
+        return query.fetch();
     }
+
 }
